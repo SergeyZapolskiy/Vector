222f098ea37c9d1062860a93025bccc89700dece
ZaPoLsKiySA@gmail.com <ZaPoLsKiySA@gmail.com>
2021-04-06 23:56:16 +0700

    Add project files.

---
+++ Vector/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(1, 4, 8);
+            Vector vector2 = new Vector(-2, 4, -9);
+            var a1 = vector1.VectorLength();
+            var a2 = vector1.ScalarComposition(vector2);
+            var a3 = vector1.VectorComposition(vector2);
+            var a4 = vector1.AngleBetweenVectors(vector2);
+            var a5 = vector1.VectorSum(vector2);
+            var a6 = vector1.VectorDif(vector2);
+            Console.WriteLine($"Vector length: {a1}\nScalar composition of vectors: {a2}\nVector composition: {a3.X}, {a3.Y}, {a3.Z}\n" +
+                $"Angle between vectors: {a4}\nVectors sum: {a5.X}, {a5.Y}, {a5.Z}\nVector dif: {a6.X}, {a6.Y}, {a6.Z}");
+            
+        }
+    }
+}
---
+++ Vector/Vector.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vector
+{
+    public class Vector
+    {
+        public Vector(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        private double _x = 0;
+
+        public double X
+        {
+            get => _x;
+        
+        }
+
+        private double _y = 0;
+
+        public double Y
+        {
+            get => _y;
+        }
+
+        private double _z = 0;
+
+        public double Z
+        {
+            get => _z;
+        }
+
+        public double VectorLength()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        public double ScalarComposition(Vector vector)
+        {
+            return (vector.X * X + vector.Y * Y + vector.Z * Z);
+        }
+
+        public Vector VectorComposition(Vector vector)
+        {
+            double Xnew = (vector.Y * Z - vector.Z * Y);
+            double Ynew = (vector.Z * X - vector.X * Z);
+            double Znew = (vector.X * Y - vector.Y * X);
+            Vector vector1 = new Vector(Xnew, Ynew, Znew);
+            return vector1; 
+        }
+
+        public double AngleBetweenVectors(Vector vector)
+        {
+            double scalar = ScalarComposition(vector);
+            double a = VectorLength();
+            double b = vector.VectorLength();
+            double c = Math.Acos(scalar / (a * b));
+            return c;
+        }
+
+        public Vector VectorSum(Vector vector)
+        {
+            Vector vector1 = new Vector(vector.X + X, vector.Y + Y, vector.Z + Z);
+            return vector1;
+        }
+
+        public Vector VectorDif(Vector vector)
+        {
+            Vector vector1 = new Vector(X - vector.X, Y - vector.Y, Z - vector.Z);
+            return vector1;
+        }
+    }
+
+}
